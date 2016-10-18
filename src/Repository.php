6ad45301e950e2d0a6d64eb2567720615885cf44
Antoine Bellion <antoinebellion@msn.com>
2016-10-18 19:48:22 +0200

    WIP

--- src/Repository.php
+++ src/Repository.php
@@ -2,6 +2,16 @@
 
 namespace Abellion\ODM\MongoDB;
 
+use MongoDB;
+
 class Repository
 {
+	public $collection;
+
+	public function __construct(MongoDB\Database $database, string $collectionName, string $entityClass)
+	{
+		$this->collection = $database->selectCollection($collectionName, [
+            "typeMap" => ["root" => $entityClass, "array" => "array", "document" => "array"]
+        ]);
+	}
 }
