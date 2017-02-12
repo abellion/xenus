86b281502e537c97b4e9c4252910b1b67b65f0d1
Antoine Bellion <antoinebellion@msn.com>
2017-02-12 21:02:58 +0100

    Collection

---
+++ src/Collection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Abellion\ODM;
+
+use MongoDB\Database;
+
+class Collection
+{
+	protected $name;
+	protected $document = Document::class;
+
+	public $collection;
+
+	public function __construct(Database $database)
+	{
+		$this->collection = $database->selectCollection($this->name, [
+			'typeMap' => ['root' => $this->document, 'document' => Document::class, 'array' => 'array']
+		]);
+	}
+
+}
