8f5bca73f9782cb9982f1d6325ddfc1d86f29b75
Antoine Bellion <antoinebellion@msn.com>
2017-02-16 22:45:17 +0100

    use sigle quotes

--- src/Document/Mutators/CamelCaseMutator.php
+++ src/Document/Mutators/CamelCaseMutator.php
@@ -6,11 +6,11 @@ trait CamelCaseMutator
 {
 	public function getterIze($offset)
 	{
-		return 'get' . str_replace(" ", "", ucwords(strtr($offset, "_-", "  ")));
+		return 'get' . str_replace(' ', '', ucwords(strtr($offset, '_-', '  ')));
 	}
 
 	public function setterIze($offset)
 	{
-		return 'set' . str_replace(" ", "", ucwords(strtr($offset, "_-", "  ")));
+		return 'set' . str_replace(' ', '', ucwords(strtr($offset, '_-', '  ')));
 	}
 }
