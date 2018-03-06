1cd8dbe8d636fb472e792f99f8b4438a476cd344
Antoine Bellion <antoinebellion@msn.com>
2018-03-06 19:47:09 +0100

    Draft relationships methods

--- src/Document/HasRelationships.php
+++ src/Document/HasRelationships.php
@@ -4,5 +4,35 @@ namespace Xenus\Document;
 
 trait HasRelationships
 {
+    protected function hasOne(string $target, string $targetKey, string $localKey = '_id')
+    {
+        $target = null;
+        $object = $this;
 
+        return new Xenus\Relations\BindOne($target, $object, $targetKey, $localKey);
+    }
+
+    protected function hasMany(string $target, string $targetKey, string $localKey = '_id')
+    {
+        $target = null;
+        $object = $this;
+
+        return new Xenus\Relations\BindMany($target, $object, $targetKey, $localKey);
+    }
+
+    protected function belongsTo(string $target, string $localKey, string $targetKey = '_id')
+    {
+        $target = null;
+        $object = $this;
+
+        return new Xenus\Relations\BindOne($target, $object, $targetKey, $localKey);
+    }
+
+    protected function belongsToMany(string $target, string $localKey, string $targetKey = '_id')
+    {
+        $target = null;
+        $object = $this;
+
+        return new Xenus\Relations\BindMany($target, $object, $targetKey, $localKey);
+    }
 }
