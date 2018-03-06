c3988f3f918e2c1a7427181da003081ac378f63d
Antoine Bellion <antoinebellion@msn.com>
2018-03-06 15:47:32 +0100

    Add doc blocks

--- src/Cursor.php
+++ src/Cursor.php
@@ -18,6 +18,11 @@ class Cursor extends IteratorIterator
         parent::__construct($iterator);
     }
 
+    /**
+     * Return the current iterator element
+     *
+     * @return mixed
+     */
     public function current()
     {
         $document = parent::current();
@@ -29,6 +34,11 @@ class Cursor extends IteratorIterator
         return $document;
     }
 
+    /**
+     * Transform the Cursor into an array
+     *
+     * @return array
+     */
     public function toArray()
     {
         $documents = [];
@@ -40,6 +50,14 @@ class Cursor extends IteratorIterator
         return $documents;
     }
 
+    /**
+     * Forward methods calls to the inner iterator
+     *
+     * @param  string $name
+     * @param  array  $arguments
+     *
+     * @return mixed
+     */
     public function __call(string $name, array $arguments)
     {
         return call_user_func_array([$this->iterator, $name], $arguments);
