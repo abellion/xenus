768e23fa5f764e80eb79d983609aca4e2dbbec6c
Antoine Bellion <antoinebellion@msn.com>
2018-03-06 19:02:43 +0100

    Naming

--- src/Relations/BindOne.php
+++ src/Relations/BindOne.php
@@ -2,11 +2,11 @@
 
 namespace Xenus\Relations;
 
-class HasMany extends AbstractRelation
+class BindOne extends AbstractRelation
 {
     public function find($filter = [], array $options = [])
     {
-        return $this->target->find(array_merge($filter, [
+        return $this->target->findOne(array_merge($filter, [
             $this->foreignKey => $this->object->get($this->primaryKey)
         ]), $options);
     }
