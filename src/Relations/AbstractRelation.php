b20647c049cb2a22acc4cebff44f253e66757a92
Antoine Bellion <antoinebellion@msn.com>
2018-03-07 12:59:37 +0100

    Use get_class instad of ::class

--- src/Relations/AbstractRelation.php
+++ src/Relations/AbstractRelation.php
@@ -23,7 +23,7 @@ abstract class AbstractRelation
         $this->primaryKey = $primaryKey;
 
         if (false === $this->object->has($this->primaryKey)) {
-            throw new Exceptions\LogicException(sprintf('The `%s` model does not have any `%s` attribute', $this->object::class, $this->primaryKey));
+            throw new Exceptions\LogicException(sprintf('The `%s` model does not have any `%s` attribute', get_class($object), $primaryKey));
         }
     }
 }
