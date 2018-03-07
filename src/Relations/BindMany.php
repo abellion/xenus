ecf2f7c1c7a7ca666ae4396911c8d0daceb57440
Antoine Bellion <antoinebellion@msn.com>
2018-03-07 11:43:21 +0100

    Query in array

--- src/Relations/BindMany.php
+++ src/Relations/BindMany.php
@@ -6,8 +6,16 @@ class BindMany extends AbstractRelation
 {
     public function find($filter = [], array $options = [])
     {
+        $subject = $this->object->get($this->primaryKey);
+
+        if (false === is_array($subject)) {
+            $query = $subject;
+        } else {
+            $query = ['$in' => $subject];
+        }
+
         return $this->target->find(array_merge($filter, [
-            $this->foreignKey => $this->object->get($this->primaryKey)
+            $this->foreignKey => $query
         ]), $options);
     }
 }
