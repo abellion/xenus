7d9871aa4dd36d7418c7c6df16783be93060925c
Antoine Bellion <antoinebellion@msn.com>
2018-03-07 20:44:10 +0100

    Update the way relationships are tested

--- tests/XenusRelationsTest.php
+++ tests/XenusRelationsTest.php
@@ -25,7 +25,7 @@ class XenusRelationsTest extends TestCase
         $this->addresses = new Addresses($this->database);
     }
 
-    public function testOneToOneRelationship()
+    public function testHasOneRelationship()
     {
         $this->users->insert(
             $user = (new User())->connect($this->users)
@@ -36,16 +36,12 @@ class XenusRelationsTest extends TestCase
         );
 
         $this->assertInstanceOf(\Xenus\Relations\BindOne::class, $user->getAddress());
-        $this->assertInstanceOf(\Xenus\Relations\BindOne::class, $address->getUser());
-
-        $this->assertInstanceOf(User::class, $address->getUser()->find());
         $this->assertInstanceOf(Address::class, $user->getAddress()->find());
 
-        $this->assertEquals((string) $user['_id'], (string) $address->getUser()->find()['_id']);
         $this->assertEquals((string) $address['_id'], (string) $user->getAddress()->find()['_id']);
     }
 
-    public function testOneToManyRelationship()
+    public function testHasManyRelationship()
     {
         $this->users->insert(
             $user = (new User())->connect($this->users)
@@ -59,20 +55,28 @@ class XenusRelationsTest extends TestCase
             $addressB = (new Address(['user_id' => $user['_id']]))->connect($this->addresses)
         );
 
-        $this->assertInstanceOf(\Xenus\Relations\BindOne::class, $addressA->getUser());
-        $this->assertInstanceOf(\Xenus\Relations\BindOne::class, $addressB->getUser());
         $this->assertInstanceOf(\Xenus\Relations\BindMany::class, $user->getAddresses());
-
-        $this->assertInstanceOf(User::class, $addressA->getUser()->find());
-        $this->assertInstanceOf(User::class, $addressB->getUser()->find());
         $this->assertInstanceOf(\Traversable::class, $user->getAddresses()->find());
-
-        $this->assertEquals((string) $user['_id'], (string) $addressA->getUser()->find()['_id']);
-        $this->assertEquals((string) $user['_id'], (string) $addressB->getUser()->find()['_id']);
 
         $addresses = $user->getAddresses()->find()->toArray();
 
         $this->assertEquals((string) $addressA['_id'], (string) $addresses[0]['_id']);
         $this->assertEquals((string) $addressB['_id'], (string) $addresses[1]['_id']);
+    }
+
+    public function testBelongsToRelationship()
+    {
+        $this->users->insert(
+            $user = (new User())->connect($this->users)
+        );
+
+        $this->addresses->insert(
+            $address = (new Address(['user_id' => $user['_id']]))->connect($this->addresses)
+        );
+
+        $this->assertInstanceOf(\Xenus\Relations\BindOne::class, $address->getUser());
+        $this->assertInstanceOf(User::class, $address->getUser()->find());
+
+        $this->assertEquals((string) $user['_id'], (string) $address->getUser()->find()['_id']);
     }
 }
