dae0b9169c8e59e89d5d062786ccf2fa1552faae
Antoine Bellion <antoinebellion@msn.com>
2018-03-08 12:04:17 +0100

    Improve tests

--- tests/XenusCollectionTest.php
+++ tests/XenusCollectionTest.php
@@ -6,126 +6,119 @@ use MongoDB\BSON\ObjectID;
 use PHPUnit\Framework\TestCase;
 
 use Xenus\Document as XenusDocument;
+use Xenus\Collection as XenusCollection;
 use Xenus\Tests\Stubs\CitiesCollection as Cities;
 
 class XenusCollectionTest extends TestCase
 {
     use Concerns\RefreshDatabase;
 
-    public function testIsMongoCollection()
-    {
-        $cities = new Cities($this->database);
+    private $cities;
 
-        $this->assertInstanceOf(\MongoDB\Collection::class, $cities);
+    public function setUp()
+    {
+        $this->createDatabase();
+
+        $this->cities = new Cities($this->database);
     }
 
-    public function testCollectionName()
+    public function testNameConfiguration()
     {
-        $cities = new Cities($this->database);
+        $collection = new class($this->database) extends XenusCollection {
+            const NAME = 'collection';
+        };
 
-        $this->assertEquals('cities', $cities->getCollectionName());
+        $this->assertEquals('collection', $collection->getCollectionName());
+
+        $collection = new class($this->database, ['name' => 'collection']) extends XenusCollection {
+        };
+
+        $this->assertEquals('collection', $collection->getCollectionName());
     }
 
-    public function testDefaultTypeMap()
+    public function testTypeMapConfiguration()
     {
-        $cities = new Cities($this->database);
+        $collection = new class($this->database) extends XenusCollection {
+            const NAME = 'collection';
+            const DOCUMENT = 'document';
+        };
 
-        $city = $cities->insertOne([
-            'name' => 'Paris',
-            'zips' => [75000, 75001, 75002],
-            'demography' => new XenusDocument()
-        ]);
+        $this->assertEquals('document', $collection->__debugInfo()['typeMap']['root']);
 
-        $city = $cities->findOne(['_id' => $city->getInsertedId()]);
+        $collection = new class($this->database, ['document' => 'document']) extends XenusCollection {
+            const NAME = 'collection';
+        };
 
-        $this->assertInstanceOf(XenusDocument::class, $city);
-        $this->assertInternalType('array', $city->zips);
-        $this->assertInternalType('array', $city->demography);
-        $this->assertInternalType('string', $city->name);
+        $this->assertEquals('document', $collection->__debugInfo()['typeMap']['root']);
     }
 
     public function testInsert()
     {
         $city = ['name' => 'Paris'];
-        $cities = new Cities($this->database);
 
-        $this->assertInstanceOf(\MongoDB\InsertOneResult::class, $cities->insert($city));
-        $this->assertInstanceOf(\MongoDB\InsertOneResult::class, $cities->insert(new XenusDocument($city)));
+        $this->assertInstanceOf(\MongoDB\InsertOneResult::class, $this->cities->insert($city));
+        $this->assertInstanceOf(\MongoDB\InsertOneResult::class, $this->cities->insert(new XenusDocument($city)));
     }
 
     public function testUpdate()
     {
         $city = ['name' => 'Paris'];
-        $cities = new Cities($this->database);
 
-        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $cities->update($city));
-        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $cities->update(new XenusDocument($city)));
+        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $this->cities->update($city));
+        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $this->cities->update(new XenusDocument($city)));
     }
 
     public function testDelete()
     {
         $city = ['name' => 'Paris'];
-        $cities = new Cities($this->database);
 
-        $this->assertInstanceOf(\MongoDB\DeleteResult::class, $cities->delete($city));
-        $this->assertInstanceOf(\MongoDB\DeleteResult::class, $cities->delete(new XenusDocument($city)));
+        $this->assertInstanceOf(\MongoDB\DeleteResult::class, $this->cities->delete($city));
+        $this->assertInstanceOf(\MongoDB\DeleteResult::class, $this->cities->delete(new XenusDocument($city)));
     }
 
     public function testFindOne()
     {
-        $cities = new Cities($this->database);
-
-        $this->assertNull($cities->findOne(new ObjectID()));
-        $this->assertNull($cities->findOne(['_id' => new ObjectID()]));
+        $this->assertNull($this->cities->findOne(new ObjectID()));
+        $this->assertNull($this->cities->findOne(['_id' => new ObjectID()]));
     }
 
     public function testDeleteOne()
     {
-        $cities = new Cities($this->database);
-
-        $this->assertInstanceOf(\MongoDB\DeleteResult::class, $cities->deleteOne(new ObjectID()));
-        $this->assertInstanceOf(\MongoDB\DeleteResult::class, $cities->deleteOne(['_id' => new ObjectID()]));
+        $this->assertInstanceOf(\MongoDB\DeleteResult::class, $this->cities->deleteOne(new ObjectID()));
+        $this->assertInstanceOf(\MongoDB\DeleteResult::class, $this->cities->deleteOne(['_id' => new ObjectID()]));
     }
 
     public function testUpdateOne()
     {
-        $cities = new Cities($this->database);
         $update = ['$set' => ['field' => 'value']];
 
-        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $cities->updateOne(new ObjectID(), $update));
-        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $cities->updateOne(['_id' => new ObjectID()], $update));
+        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $this->cities->updateOne(new ObjectID(), $update));
+        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $this->cities->updateOne(['_id' => new ObjectID()], $update));
     }
 
     public function testReplaceOne()
     {
-        $cities = new Cities($this->database);
-
-        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $cities->replaceOne(new ObjectID(), []));
-        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $cities->replaceOne(['_id' => new ObjectID()], []));
+        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $this->cities->replaceOne(new ObjectID(), []));
+        $this->assertInstanceOf(\MongoDB\UpdateResult::class, $this->cities->replaceOne(['_id' => new ObjectID()], []));
     }
 
     public function testFindOneAndDelete()
     {
-        $cities = new Cities($this->database);
-
-        $this->assertNull($cities->findOneAndDelete(new ObjectID()));
-        $this->assertNull($cities->findOneAndDelete(['_id' => new ObjectID()]));
+        $this->assertNull($this->cities->findOneAndDelete(new ObjectID()));
+        $this->assertNull($this->cities->findOneAndDelete(['_id' => new ObjectID()]));
     }
 
     public function testFindOneAndUpdate()
     {
-        $cities = new Cities($this->database);
         $update = ['$set' => ['field' => 'value']];
 
-        $this->assertNull($cities->findOneAndUpdate(new ObjectID(), $update));
-        $this->assertNull($cities->findOneAndUpdate(['_id' => new ObjectID()], $update));
+        $this->assertNull($this->cities->findOneAndUpdate(new ObjectID(), $update));
+        $this->assertNull($this->cities->findOneAndUpdate(['_id' => new ObjectID()], $update));
     }
 
     public function testFindOneAndReplace()
     {
-        $cities = new Cities($this->database);
-
-        $this->assertNull($cities->findOneAndReplace(new ObjectID(), []));
-        $this->assertNull($cities->findOneAndReplace(['_id' => new ObjectID()], []));
+        $this->assertNull($this->cities->findOneAndReplace(new ObjectID(), []));
+        $this->assertNull($this->cities->findOneAndReplace(['_id' => new ObjectID()], []));
     }
 }
