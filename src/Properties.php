5756750c4612a54cea1abda324f10e60e65da6aa
Antoine Bellion <antoinebellion@msn.com>
2020-04-26 15:00:29 +0200

    Add properties methods

--- src/Properties.php
+++ src/Properties.php
@@ -18,6 +18,51 @@ class Properties
     }
 
     /**
+     * Get the collection's options
+     *
+     * @return Array
+     */
+    public function getCollectionOptions()
+    {
+        $options = ['typeMap' => ['root' => $this->properties->document, 'array' => 'array', 'document' => 'array']];
+
+        if (isset($this->properties->options)) {
+            return array_merge($this->properties->options, $options);
+        }
+
+        return $options;
+    }
+
+    /**
+     * Get the collection's name
+     *
+     * @return String|null
+     */
+    public function getCollectionName()
+    {
+        if (isset($this->properties['name'])) {
+            return $this->properties['name'];
+        }
+
+        return null;
+    }
+
+    /**
+     * Determine if the properties have the given proeprty
+     *
+     * @param  string $property
+     *
+     * @return bool
+     */
+    public function have(string $property) {
+        if (isset($this->properties[$property])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
      * Build the properties based on the given object
      *
      * @param  mixed $collection
