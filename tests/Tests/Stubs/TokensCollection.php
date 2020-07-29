095ca3f699b6499e82709ecee6f7262644de6415
Antoine Bellion <antoinebellion@msn.com>
2020-07-29 17:28:00 +0200

    Create stub collection to test the ED

---
+++ tests/Tests/Stubs/TokensCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Xenus\Tests\Tests\Stubs;
+
+class TokensCollection extends \Xenus\Collection
+{
+    protected $name = 'tokens';
+    protected $document = \Xenus\Document::class;
+
+    /**
+     * Set the event at the given hook
+     *
+     * @param  string $hook
+     * @param  string $event
+     *
+     * @return self
+     */
+    public function setEvent(string $hook, string $event)
+    {
+        $this->events[$hook] = $event;
+
+        return $this;
+    }
+}
+
