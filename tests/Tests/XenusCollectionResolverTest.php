836544aab1cd09e3d3430cc18da8be9072734849
Antoine Bellion <antoinebellion@msn.com>
2020-07-30 14:13:54 +0200

    Add tests

---
+++ tests/Tests/XenusCollectionResolverTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Xenus\Tests\Tests;
+
+use Xenus\Tests\Tests\Stubs\UsersCollection;
+use Xenus\Tests\Tests\Stubs\CitiesCollection;
+use Xenus\Tests\Mocks\CollectionResolverMock;
+
+use Xenus\Connection;
+
+class XenusCollectionResolverTest extends \PHPUnit\Framework\TestCase
+{
+    public function test_custom_resolver_is_used()
+    {
+        $cities = (new CitiesCollection(new Connection('mongodb://xxx', 'xxx')))->setCollectionResolver(
+            $resolver = new CollectionResolverMock()
+        );
+
+        $cities->resolve(UsersCollection::class);
+
+        $this->assertContains(
+            UsersCollection::class, $resolver->resolved
+        );
+    }
+}
